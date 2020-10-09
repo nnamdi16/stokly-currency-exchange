4848cd94961db26a285b54100a04fd36774b3b3b
Nnamdi <nnwabuokei@paga.com>
2020-10-09 21:33:39 +0100

    create an exchange-rate service that makes an API call to exchangeratesapi

---
+++ src/app/services/exchange-rate.service.ts
@@ -0,0 +1,23 @@
+import { ExchangeRate } from './../models/ExchangeRate';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-type':'application/json',
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExchangeRateService {
+  exchangeRateUrl : string = 'https://api.exchangeratesapi.io/latest';
+  constructor(private http:HttpClient) { }
+
+  // Get latest exchange rate
+  getLatestExchangeRate(): Observable<ExchangeRate> {
+    return this.http.get<ExchangeRate>(this.exchangeRateUrl);
+  }
+}
