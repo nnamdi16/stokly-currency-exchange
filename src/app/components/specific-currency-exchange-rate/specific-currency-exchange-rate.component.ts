6140e736a5c8d535efcf3120c407729d51e99f1c
Nnamdi <nnwabuokei@paga.com>
2020-10-10 12:01:13 +0100

    implemented logic for specific currency exchange rate

---
+++ src/app/components/specific-currency-exchange-rate/specific-currency-exchange-rate.component.ts
@@ -0,0 +1,28 @@
+import { SpecificCurrencyExchangeRateService } from './../../services/specific-currency-exchange-rate.service';
+import { Component, OnInit } from '@angular/core';
+import { ExchangeRate } from 'src/app/models/ExchangeRate';
+
+@Component({
+  selector: 'app-specific-currency-exchange-rate',
+  templateUrl: './specific-currency-exchange-rate.component.html',
+  styleUrls: ['./specific-currency-exchange-rate.component.css']
+})
+export class SpecificCurrencyExchangeRateComponent implements OnInit {
+  specificCurrencyExchangeRate: ExchangeRate;
+
+  constructor(private specificCurrencyExchangeRateService:SpecificCurrencyExchangeRateService) { }
+
+  ngOnInit(): void {
+    this.specificCurrencyExchangeRate = new ExchangeRate();
+    this.getSpecificCurrencyExchangeRate();
+  }
+
+  getSpecificCurrencyExchangeRate() {
+    this.specificCurrencyExchangeRateService.getSpecificCurrencyExchangeRate().subscribe(specificCurrencyExchangeRate => {
+      console.log( `Specific Country Exchange Rate`, specificCurrencyExchangeRate);
+      this.specificCurrencyExchangeRate = specificCurrencyExchangeRate;
+    });
+   
+  }
+
+}
