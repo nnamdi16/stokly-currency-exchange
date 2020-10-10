db99045e56fd5818821f9acfeef9dc6c7e316b4d
Nnamdi <nnwabuokei@paga.com>
2020-10-10 21:40:27 +0100

    added FormsModule and OtherCurrencyExchangeRate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExchangeRateComponent } from './components/exchange-rate/exchange-rate.component';
 import { HistoricalExchangeRateComponent } from './components/historical-exchange-rate/historical-exchange-rate.component';
+import { OtherCurrencyExchangeRateConversionComponent } from './components/other-currency-exchange-rate-conversion/other-currency-exchange-rate-conversion.component';
 import { SelectedCurrenciesExchangeRateComponent } from './components/selected-currencies-exchange-rate/selected-currencies-exchange-rate.component';
 import { SelectedCurrenciesHistoricalExchangeRateComponent } from './components/selected-currencies-historical-exchange-rate/selected-currencies-historical-exchange-rate.component';
 import { SpecificCurrencyExchangeRateComponent } from './components/specific-currency-exchange-rate/specific-currency-exchange-rate.component';
 import { SpecificHistoricalExchangeRateComponent } from './components/specific-historical-exchange-rate/specific-historical-exchange-rate.component';
-import { OtherCurrencyExchangeRateConversionComponent } from './components/other-currency-exchange-rate-conversion/other-currency-exchange-rate-conversion.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { OtherCurrencyExchangeRateConversionComponent } from './components/other
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
