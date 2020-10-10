e13c44f0842c04ab19213007616ff9d09da125c9
Nnamdi <nnwabuokei@paga.com>
2020-10-10 03:48:18 +0100

    implememented logic for historical exchange rate

---
+++ src/app/components/historical-exchange-rate/historical-exchange-rate.component.ts
@@ -0,0 +1,29 @@
+import { HistoricalExchangeRate } from './../../models/HistoricalExchangeRate';
+import { Component, OnInit } from '@angular/core';
+import { ExchangeRate } from 'src/app/models/ExchangeRate';
+import { HistoricalExchangeRateService } from './../../services/historical-exchange-rate.service';
+
+@Component({
+  selector: 'app-historical-exchange-rate',
+  templateUrl: './historical-exchange-rate.component.html',
+  styleUrls: ['./historical-exchange-rate.component.css']
+})
+export class HistoricalExchangeRateComponent implements OnInit {
+  historicalExchangeRate : HistoricalExchangeRate
+
+  constructor(private historicalexchangeRateService:HistoricalExchangeRateService) { }
+
+  ngOnInit(): void {
+    this.historicalExchangeRate = new HistoricalExchangeRate();
+    this.getHistoricalExchangeRate();
+  }
+
+  getHistoricalExchangeRate(){
+    this.historicalexchangeRateService.getHistoricalExchangeRate().subscribe(historicalExchangeRate => {
+      console.log(historicalExchangeRate);
+      this.historicalExchangeRate = historicalExchangeRate;
+    })
+
+  }
+
+}
