ccdacf9f0df4ee8a0bc6cf64f86d17dae95179b0
Nnamdi <nnwabuokei@paga.com>
2020-10-11 14:28:20 +0100

    added the startDate property,endDate property which are required as params and the loading and errorMessage properties. Refactored the getHistoricalExchangeRate method

--- src/app/components/historical-exchange-rate/historical-exchange-rate.component.ts
+++ src/app/components/historical-exchange-rate/historical-exchange-rate.component.ts
@@ -1,5 +1,6 @@
-import { HistoricalExchangeRate } from './../../models/HistoricalExchangeRate';
 import { Component, OnInit } from '@angular/core';
+import { Error } from './../../models/Error';
+import { HistoricalExchangeRate } from './../../models/HistoricalExchangeRate';
 import { HistoricalExchangeRateService } from './../../services/historical-exchange-rate.service';
 
 @Component({
@@ -8,20 +9,39 @@ import { HistoricalExchangeRateService } from './../../services/historical-excha
   styleUrls: ['./historical-exchange-rate.component.css']
 })
 export class HistoricalExchangeRateComponent implements OnInit {
+  startDate:string ='';
+  endDate:string = '';
   historicalExchangeRate : HistoricalExchangeRate;
+  loading:boolean = false;
+  errorMessage:Error;
 
   constructor(private historicalexchangeRateService:HistoricalExchangeRateService) { }
 
   ngOnInit(): void {
     this.historicalExchangeRate = new HistoricalExchangeRate();
-    this.getHistoricalExchangeRate();
   }
 
   getHistoricalExchangeRate(){
-    this.historicalexchangeRateService.getHistoricalExchangeRate().subscribe(historicalExchangeRate => {
-      console.log('Historical Exchange Rate',historicalExchangeRate);
-      this.historicalExchangeRate = historicalExchangeRate;
-    })
+    this.loading = true;
+    this.errorMessage = new Error();
+    this.historicalexchangeRateService.getHistoricalExchangeRate(this.startDate,this.endDate)
+      .subscribe(
+        historicalExchangeRate => {
+        console.log('Historical Exchange Rate',historicalExchangeRate);
+        this.historicalExchangeRate = historicalExchangeRate;
+        
+    },
+      (error) => {
+        console.log('Request failed with error');
+        this.errorMessage = error;
+        this.loading = false;
+    },
+      () => {
+        console.log('Request Completed');
+        this.loading = false;
+      }
+    
+    )
 
   }
 
