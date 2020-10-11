d877a5fdba918d86104b022a52b918999757afb9
Nnamdi <nnwabuokei@paga.com>
2020-10-11 14:37:21 +0100

    added HttpParams to pass parameters to the base url

--- src/app/services/historical-exchange-rate.service.ts
+++ src/app/services/historical-exchange-rate.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HistoricalExchangeRate } from './../models/HistoricalExchangeRate';
@@ -14,12 +14,17 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class HistoricalExchangeRateService {
-
+  baseUrl = `https://api.exchangeratesapi.io/history`;
   exchangeRateUrl : string = 'https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01';
   constructor(private http:HttpClient) { }
 
     // Get historical exchange rate
-    getHistoricalExchangeRate(): Observable<HistoricalExchangeRate> {
-      return this.http.get<HistoricalExchangeRate>(this.exchangeRateUrl);
+    getHistoricalExchangeRate(startDate:string,endDate:string): Observable<HistoricalExchangeRate> {
+      let params = new HttpParams()
+        .set('start_at',startDate)
+        .set('end_at',endDate);
+      
+      console.log(params.toString);
+      return this.http.get<HistoricalExchangeRate>(this.baseUrl,{params});
     }
 }
