c6b44f5686e47a31928049af035020e2cc91ddeb
Nnamdi <nnwabuokei@paga.com>
2020-10-13 21:00:05 +0100

    removed unwanted components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,6 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExchangeRateComponent } from './components/exchange-rate/exchange-rate.component';
-import { HistoricalExchangeRateComponent } from './components/historical-exchange-rate/historical-exchange-rate.component';
-import { OtherCurrencyExchangeRateConversionComponent } from './components/other-currency-exchange-rate-conversion/other-currency-exchange-rate-conversion.component';
-import { SelectedCurrenciesExchangeRateComponent } from './components/selected-currencies-exchange-rate/selected-currencies-exchange-rate.component';
-import { SelectedCurrenciesHistoricalExchangeRateComponent } from './components/selected-currencies-historical-exchange-rate/selected-currencies-historical-exchange-rate.component';
-import { SpecificCurrencyExchangeRateComponent } from './components/specific-currency-exchange-rate/specific-currency-exchange-rate.component';
-import { SpecificHistoricalExchangeRateComponent } from './components/specific-historical-exchange-rate/specific-historical-exchange-rate.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
@@ -23,12 +17,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   declarations: [
     AppComponent,
     ExchangeRateComponent,
-    HistoricalExchangeRateComponent,
-    SpecificHistoricalExchangeRateComponent,
-    SpecificCurrencyExchangeRateComponent,
-    SelectedCurrenciesExchangeRateComponent,
-    SelectedCurrenciesHistoricalExchangeRateComponent,
-    OtherCurrencyExchangeRateConversionComponent,
     DashboardComponent,
     NavbarComponent
   ],
